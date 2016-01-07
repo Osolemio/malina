e46b051b229abb59f69e7b0eb62c1a5fcf09e773
Osolemio <osolemio@live.ru>
2016-01-07 22:23:32 +0300

    07012016_22:20 commit

--- html/errors_table.php
+++ html/errors_table.php
@@ -8,9 +8,12 @@
 
     
 </head>
+<style>
+table {table-layout:fixed}
 
-
+</style>
 <body>
+
 
 <?php
     session_start();
@@ -39,7 +42,7 @@
 			'Выключились по многократным КЗ по заряду, через 2ч снимаем ошибку.'
 			    ),
 	    '_RSWarning'=>array(
-			'Нет действия для комбинации кнопка (“СТАРТ” и “ЗАРЯД”), можно игнорировать.',
+			'Нет действия для комбинации кнопка (“СТАРТ” и “ЗАРЯД”)',
 			'Сетевое напряжение вышло за пределы.',
 			'Есть постороннее напряжение на выходной розетке или большие выбросы напряжения от нагрузки.',
 			'Выбросы напряжения в сети 220В по входу.',
@@ -67,11 +70,33 @@
 			'н/о',
 			'н/о',
 			'н/о'
+			    ),
+	    '_F_AccOver'=>array(
+			'Отключение по перегрузке по току АКБ, 10 попыток перезапуска.',
+			'Отключение по перегрузке по току АКБ во время заряда, 10 попыток перезапуска.',
+			'Отключение по перегрузке по S_UAccLow - критически низкое напряжение АКБ, снимается при восcтановлении напряжения на АКБ',
+			'Отключение по неисправности вентилятора, 10 попыток перезапуска.',
+			'Отключение по мощности выше номинальной более 30мин - снимается через 30мин',
+			'Отключение по полному разряду АКБ - снимаем по достижению напряжения на АКБ выше EE_LCD_UAccMinNetGen =12,5в (заводское) или при переходе на заряд',
+			'Отключение по выходу за пределы датчика температуры, снимается при остывании',
+			'Полное отключение по многократным перегрузкам по АКБ, снимается персоналом кнопкой “СТАРТ”'
+			    ),
+	'_F_NETOver'=>array(
+			'Отключение генерации - на выходе постороннее напряжение или неисправное реле. Либо не правильное подключение проводов 220В либо ремонт.',
+			'Переход на генерацию - перегрузка по сети (превышена максимальная мощность EE_LCD_NetMaxPow порядка 1,5 максимальной мощности МАП) , снимается после исчезновения сети со входа но не более 10 раз в течении 10 мин.',
+			'н/о',
+			'Выключение по многократным перегрузкам по сети, снимается персоналом кнопкой “СТАРТ”',
+			'н/о',
+			'н/о',
+			'н/о',
+			'н/о'
 			    )
+
+    
 	);
 
 	    $data_mppt=array(
-	     'RSErrSis'=>array(
+	     '_RSErrSis'=>array(
 			'Перенапряжение панелей',
 			'Перенапряжение АКБ',
 			'Перезаряд АКБ',
@@ -85,24 +110,116 @@
 	);
 
 	   $legend=array('map_errors'=>'МАП', 'mppt_errors'=>'MPPT');
+?>
+<b>
+<hr>
+<center>История ошибок <?php echo $legend[$_SESSION['table']]; ?></center>
+<hr>
+
+<table align="center" width="100%" bgcolor="ivory" border="2px" cellpadding="2px">
+<tr bgcolor="blanchedalmond"><td>Дата</td><td>Время</td><td>_RSErrSis</td>
+<?php
+    if ($_SESSION['table']=='map_errors')
+
+	 echo "<td>_RSErrJobM</td><td>_RSWarning</td><td>_I2CErr</td><td>_RSErrDop</td><td>Состояние по перегрузке АКБ</td><td>Состояние по перегрузке по сети 220</td><td>Частота вне диапазона, Гц</td><td>Напряжение вне диапазона, В</td></tr></b>";
+else echo "</tr></b>";
+
+
+	
 
 	while ($row=mysql_fetch_assoc($result))
 	    {
+	echo "<tr><td>".$row['date']."</td><td>".$row['time']."</td><td>";
+	
+          if ($_SESSION['table']=='mppt_errors')
+	    {
+	      echo "Value=".$row['_RSErrSis']."&nbsp";    
+	     for ($i=1;$i<=8;$i++)  if (($row['_RSErrSis']&$i)==$i) echo $data_mppt['_RSErrSis'][$i-1].",";
+	      echo "</td>";
+	    }
+	
+	  if ($_SESSION['table']=='map_errors')	     
+	    {
+	     echo "Value=".$row['_RSErrSis']."&nbsp";
+	     for ($i=1;$i<=8;$i++)  if (($row['_RSErrSis']&$i)==$i) echo $data_map['_RSErrSis'][$i-1].",";
+	     echo "</td><td>";
 	    
+	    echo "Value=".$row['_RSErrJobM']."&nbsp";
+	     for ($i=1;$i<=8;$i++)  if (($row['_RSErrJobM']&$i)==$i) echo $data_map['_RSErrJobM'][$i-1].",";
+	     echo "</td><td>";
+
+	    echo "Value=".$row['_RSWarning']."&nbsp";
+	     for ($i=1;$i<=8;$i++)  if (($row['_RSWarning']&$i)==$i) echo $data_map['_RSWarning'][$i-1].",<br>";
+	     echo "</td><td>";
+
+	    echo "Value=".$row['_I2C_Err']."&nbsp";
+	     for ($i=1;$i<=8;$i++)  if (($row['_I2C_Err']&$i)==$i) echo $data_map['_I2C_Err'][$i-1].",";
+	     echo "</td><td>";
+
+	echo "Value=".$row['_RSErrDop']."&nbsp";
+	     for ($i=1;$i<=8;$i++)  if (($row['_RSErrDop']&$i)==$i) echo $data_map['_RSErrDop'][$i-1].",";
+	     echo "</td><td>";
+
+	echo "Value=".$row['_F_AccOver']."&nbsp";
+	     for ($i=1;$i<=8;$i++)  if (($row['_F_AccOver']&$i)==$i) echo $data_map['_F_AccOver'][$i-1].",";
+	     echo "</td><td>";
+
+	echo "Value=".$row['_F_NETOver']."&nbsp";
+	     for ($i=1;$i<=8;$i++)  if (($row['_F_NETOver']&$i)==$i) echo $data_map['_F_NETOver'][$i-1].",";
+	     echo "</td><td>";
+
+
+	echo "Value=".round(2500/$row['_TFNET_Limit'],1)."&nbsp</td><td>";
+	echo "Value=".$row['_UNET_Limit']."&nbsp</td>";
 
 
 
-
-
-
-
-
+	    }
+	
+	echo "</tr>";
 	    }
 
 
     mysql_free_result($result);
     mysql_close($db);
 ?>
+</table>
+<div>
+<input TYPE="button" style="font-weight:bolder; background-color:darkkhaki;" VALUE=" МЕНЮ " ONCLICK="HomeButton()"> 
+<input TYPE="button" style="font-weight:bolder; background-color:darkkhaki;" VALUE=" СИСТЕМА " ONCLICK="SystemButton()"> 
+<input TYPE="button" style="font-weight:bolder; background-color:darkkhaki;" VALUE=" ТЕКСТ " ONCLICK="TextButton()"> 
+<input TYPE="button" style="font-weight:bolder; background-color:darkkhaki;" VALUE=" ПРИБОРЫ " ONCLICK="GaugesButton()"> 
+
+<script>
+
+function HomeButton()
+{
+location.href="menu.php";
+}
+
+function SystemButton()
+{
+location.href="/setup/index.php";
+}
+
+function TextButton()
+{
+location.href="index.php";
+}
+
+function GaugesButton()
+{
+location.href="gauges.php";
+}
+
+
+
+</script>
+
+
+
+</div>
+
 
 </body>
 </html>
