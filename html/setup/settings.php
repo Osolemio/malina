ca059a5b7692bd0e4bfb290b89c20854a3408f4b
Osolemio <osolemio@live.ru>
2016-01-10 01:44:21 +0300

    10012016_01:45 commit

--- html/setup/settings.php
+++ html/setup/settings.php
@@ -19,7 +19,7 @@
 <body>
 
 
-<input TYPE="button" style="font-weight:bolder; background-color:darkkhaki;" VALUE=" МЕНЮ " ONCLICK="HomeButton()"> 
+<input TYPE="button" style="font-weight:bolder; background-color:darkkhaki;cursor:pointer;" VALUE=" МЕНЮ " ONCLICK="HomeButton()"> 
 
 <script>
 function HomeButton()
@@ -34,7 +34,7 @@ location.href="index.php";
 <form action="store.php" method="post">
 
 <center>
- <select size=6 id="chapter" name="Menu" style="width:80%; font-weight:bolder; background:aqua;">
+ <select size=6 id="chapter" name="Menu" style="width:80%; font-weight:bolder; background:aqua;cursor:pointer;">
     <option value=0>Генерация МАП</option>
     <option value=1>Сеть/Генератор, BMS/MPPT</option>
     <option value=2>Сеть/ЭнергЭконом</option>
@@ -44,9 +44,9 @@ location.href="index.php";
     </select>
 </center>
  <br><br>    
-  <select id="menu" style="width:80%; background: ivory;"></select>
+  <select id="menu" style="width:80%; background: ivory;cursor:pointer;"></select>
   <br><br>
-  <select id="submenu" style="width:80%; background: ivory;"></select>
+  <select id="submenu" style="width:80%; background: ivory;cursor:pointer;"></select>
 
 <input type="hidden" name="mem_offset" id="mem_offset">
 <input type="hidden" name="mem_value" id="mem_value">
@@ -61,7 +61,7 @@ location.href="index.php";
 Текущее значение:
 <input type="number" name="field" id="input_field" value="">
 <br><br>
-<input type="submit" value="Записать" style="padding:1px; font-weight:bolder; font-size:100%; background:green; color:ivory;">
+<input type="submit" value="Записать" style="padding:1px; font-weight:bolder; font-size:100%; background:green; color:ivory; cursor:pointer;">
 </div>
 </b>
 </form>
@@ -78,11 +78,11 @@ location.href="index.php";
 
 <form action="commands.php" method="post">
 
-<p><input type="submit" name="on" value="включить МАП" style="height: 50px; width: 60%; font-weight:bolder; font-size:130%; background:green; color:ivory;" ></p>
-<p><input type="submit" name="off" value="выключить МАП" style="height: 50px; width: 60%; font-weight:bolder; font-size:130%; background:red; color:ivory;"></p>
-<p><input type="submit" name="reset" value="сброс МАП" style="height: 50px; width: 60%; font-weight:bolder; font-size:130%; background:red; color:ivory;"></p>
-<p><input type="submit" name="charge_start" value="включить заряд" style="height: 50px; width: 60%; font-weight:bolder; font-size:130%; background:darkblue; color:ivory;" ></p>
-<p><input type="submit" name="charge_stop" value="выключить заряд" style="height: 50px; width: 60%; font-weight:bolder; font-size:130%; background:darkblue; color:ivory;"></p>
+<p><input type="submit" name="on" value="включить МАП" style="height: 50px; width: 60%; font-weight:bolder; font-size:130%; background:green; color:ivory;cursor:pointer;" ></p>
+<p><input type="submit" name="off" value="выключить МАП" style="height: 50px; width: 60%; font-weight:bolder; font-size:130%; background:red; color:ivory;cursor:pointer;"></p>
+<p><input type="submit" name="reset" value="сброс МАП" style="height: 50px; width: 60%; font-weight:bolder; font-size:130%; background:red; color:ivory;cursor:pointer;"></p>
+<p><input type="submit" name="charge_start" value="включить заряд" style="height: 50px; width: 60%; font-weight:bolder; font-size:130%; background:darkblue; color:ivory;cursor:pointer;" ></p>
+<p><input type="submit" name="charge_stop" value="выключить заряд" style="height: 50px; width: 60%; font-weight:bolder; font-size:130%; background:darkblue; color:ivory;cursor:pointer;"></p>
 
 </form></b>
 </center>
