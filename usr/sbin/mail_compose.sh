557adc0e5f07f809b4e4bc30efd064a6935d4c1f
Osolemio <osolemio@live.ru>
2016-02-05 17:08:21 +0300

    05022016_17:10 commit

--- usr/sbin/mail_compose.sh
+++ usr/sbin/mail_compose.sh
@@ -10,6 +10,7 @@ declare -A max
 
 #here you have to define fields to check, min & max values, their aliases for mail, your email, items number.
 
+#================= Values from mysql DB===================
 #_Uacc
 data_fields[1]=${out[5]}
 #_UOUTmed
@@ -19,44 +20,51 @@ data_fields[3]=${out[3]}
 #_Temp_Grad0
 data_fields[4]=${out[22]}
 
+#=============  Aliases or text============================
 alias[1]="Acc voltage "
 alias[2]="Output AC "
-alias[3]="Input AC "
+alias[3]="Mode "
 alias[4]="Acc temp "
 
+
+#=============== min & max values ========================
 min[1]=44.0
 min[2]=200
-min[3]=200
+
+# MODE=2, NO INPUT AC
+#if min=max, alert will be on equal. Otherwise >=max or <=min
+# you may use decimals because we use perl to compare 
+
+min[3]=2
 min[4]=3
 
 max[1]=59.0
 max[2]=250
-max[3]=250
+max[3]=2
 max[4]=30
 
+#====================== 
 # Number of items
 index=4
 
+#====================== Where we want to send email to =========================
+mail_recipient="12345@mail.ru"
 
-mail_recipient="ivan_belarus@inbox.ru"
-from_email="microart_test@mail.ru"
+#============================== script body ===================================
 
 for ((i=1; i<=$index; i++)) do
 
-  result=(`perl -e "if(${data_fields[$index]}>=${max[$index]} || ${data_fields[$index]}<=${min[$index]}){print 0;}else{print 1;}"`)
+  result=(`perl -e "if(${data_fields[$i]}<${max[$i]} || ${data_fields[$i]}>${min[$i]}){print 1;}else{print 0;}"`)
 
 if test $result -eq 0; then
 
  if test ! -e "/var/map/temp_email_$i"; then
     cur_t=`date`
-    echo "To: $mail_recipient" > /var/map/temp_email_$i
-    echo "From: $from_email" > /var/map/temp_email_$i
-    echo "Subject: email from MAP" > /var/map/temp_email_$i
     echo " " >> /var/map/temp_email_$i
-    echo $text >> /var/map/temp_email_$i
-    echo "value: ${data_fields[$index]} $cur_t" >> /var/map/temp_email_$i
+    echo ${alias[$i]} >> /var/map/temp_email_$i
+    echo "value: ${data_fields[$i]} $cur_t" >> /var/map/temp_email_$i
     tmp_name=`date +%s`
-    /usr/sbin/ssmtp -t < /var/map/temp_email_$i
+    /usr/bin/mail -s "Notification from MAP" $mail_recipient < /var/map/temp_email_$i
  fi
  fi
 
@@ -65,14 +73,11 @@ if test $result -eq 0; then
  if test -e "/var/map/temp_email_$i"; then
     cur_t=`date`
     rm /var/map/temp_email_$i
-    echo "To: $mail_recipient" > /var/map/temp_email_ok_$i
-    echo "From: $from_email" > /var/map/temp_email_ok_$i
-    echo "Subject: email from MAP" > /var/map/temp_email_ok_$i
     echo " " >> /var/map/temp_email_ok_$i
     echo $text >> /var/map/temp_email_ok_$i
-    echo "value: ${alias[$index]} is in range $cur_t" >> /var/map/temp_email_ok_$i
+    echo "value: ${alias[$i]} is in range. Value=${data_fields[$i]} $cur_t" >> /var/map/temp_email_ok_$i
     tmp_name=`date +%s`
-    /usr/sbin/ssmtp -t < /var/map/temp_email_ok_$i
+    /usr/bin/mail -s "Notification from MAP" $mail_recipient < /var/map/temp_email_ok_$i
     rm /var/map/temp_email_ok_$i
  fi
  fi
