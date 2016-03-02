b13faf23755cd561bf06b4d2ae9feeb23bd2d212
Osolemio <osolemio@live.ru>
2016-03-02 20:53:59 +0300

    02032016_20:55 commit

--- html/multichart.php
+++ html/multichart.php
@@ -6,6 +6,7 @@
   <script src="./js/jquery-2.1.3.min.js"></script>
   <script src="./js/jquery-migrate-1.2.1.min.js"></script>
   <script src="./zingchart/zingchart.min.js"></script>
+<?php include('./local/local_ru.inc');?>
   <script src="./multichart.js"></script>
 
   <title>MAP Monitor. Diagrams</title>
@@ -20,14 +21,14 @@
   <input type="checkbox" id="menu-collapsed" name="menu-collapsed" checked/>
   <div class="menu-content">
     <ul>
-      <li><a href="index.php">ТЕКСТ</a></li>
-      <li><a href="graph.php">МОЩНОСТИ</a></li>
-      <li><a href="history.php">ИСТОРИЯ</a></li>
-      <li><a href="multi_select.php">МУЛЬТИГРАФ</a></li>
-      <li><a href="bms.php">BMS</a></li>
-      <li><a href="settings.php">ИНФО</a></li>
-      <li><a href="./setup/settings.php">СИСТЕМА-МАП</a></li>
-      <li><a href="./setup/sys.php">СИСТЕМА-СЕРВИСЫ</a></li>
+      <li><a href="index.php"><?php loc('TEXT');?></a></li>
+      <li><a href="graph.php"><?php loc('POWERS');?></a></li>
+      <li><a href="history.php"><?php loc('HISTORY');?></a></li>
+      <li><a href="multi_select.php"><?php loc('MULTICHART');?></a></li>
+      <li><a href="bms.php"><?php loc('BMS');?></a></li>
+      <li><a href="settings.php"><?php loc('INFO');?></a></li>
+      <li><a href="./setup/settings.php"><?php loc('SYSTEM');?>-<?php loc('MAC');?></a></li>
+      <li><a href="./setup/sys.php"><?php loc('SYSTEM');?>-<?php loc('SERVICES');?></a></li>
     
     </ul>
   </div>
@@ -45,7 +46,7 @@
 
   if (isset($_POST['imap'])) { echo "<div id='chart_inet'  class='charts'>
   <div id='button_pnet'>
-  <button onclick='pnet_switch();'><b>Мощность/ток</b></button>
+  <button onclick='pnet_switch();'><b>".$text['p-i']."</b></button>
   </div></div>";}
 }
 
@@ -54,11 +55,11 @@ if (file_exists("/var/map/.mppt") && isset($_POST['imppt']))
   echo '<div id="chart_ipv" class="charts"></div>';
 
 if (file_exists("/var/map/.map") && isset($_POST['iacc'])) {?>
-  <button onclick="pbal_switch();"><b>Мощность/ток</b></button>
+  <button onclick="pbal_switch();"><b><?php loc('p-i');?></b></button>
   </div></div>
-  <button onclick="sw_map();"><b>МАП</b></button>
+  <button onclick="sw_map();"><b><?php loc('MAC');?></b></button>
   </div></div>
-  <button onclick="sw_mppt();"><b>MPPT</b></button>
+  <button onclick="sw_mppt();"><b><?php loc('MPPT');?></b></button>
   </div></div>
   <div id="chart_balance"  class='charts'></div>
 <?php }
