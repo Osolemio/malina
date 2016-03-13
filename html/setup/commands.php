5d3a165e32631aafa8dac3df53441fde33adba5f
Osolemio <osolemio@live.ru>
2016-03-13 15:12:29 +0300

    13032016_15:10 commit

--- html/setup/commands.php
+++ html/setup/commands.php
@@ -1,5 +1,7 @@
 <?php
 
+include('../local/local_ru.php');
+
 if (isset($_POST['on'])) $value=chr(2);;
 if (isset($_POST['off'])) $value=chr(1);
 if (isset($_POST['charge_start'])) $value=chr(5);
@@ -23,9 +25,9 @@ if (isset($_POST['relay_1']) || isset($_POST['relay_2']))
 	}
 
 if (!isset($_POST['confirm'])) {
-    echo "Вы не отметили флажок отправки команды <br>";
+    loc('checkbox_required');
     header("Refresh:2; URL=".$_SERVER['HTTP_REFERER']);
-    echo "Через 2 секунды вы будете возвращены обратно";
+    loc('2sec_return');
 
     exit(-1);
 
@@ -42,10 +44,9 @@ $to_map=fopen("/var/map/to_map","w");
 
 if (file_exists("/var/map/.restricted") && (isset($_POST['off']) || isset($_POST['reset'])))
     {
-    echo "Включен режим ограничения. Вы не можете менять эту настройку <br>";
+    loc('restricted_mode');
     header("Refresh:2; URL=".$_SERVER['HTTP_REFERER']);
-    echo "Через 2 секунды вы будете возвращены обратно";
-
+    loc('2sec_return');
     exit(-1);
     }
 
@@ -63,7 +64,8 @@ if (isset($_POST['relay_1']) || isset($_POST['relay_2']))
 
 
 fwrite($to_map, $post);
-echo "Команда передана.<br>";
+loc('command_succes');
+echo "<br>";
 fclose($to_map);
 
  do {
@@ -76,10 +78,10 @@ mysql_free_result($result);
 $result=mysql_query("TRUNCATE TABLE eeprom_result",$db) or die(mysql_error());     
 mysql_free_result($result);
 mysql_close($db);
-
-echo "Команда записана в МАП <br>";
+loc('command_stored');
+echo "<br>";
 header("Refresh:2; URL=".$_SERVER['HTTP_REFERER']);
-echo "Через 2 секунды вы будете возвращены обратно";
+loc('2sec_return');
 exit;
 
 ?>
