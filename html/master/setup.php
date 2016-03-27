9d9225a53cfb49706ac7d5eb029ef519b4a9399a
Osolemio <osolemio@live.ru>
2016-03-27 14:28:49 +0300

    27032016_14:25 commit

--- html/master/setup.php
+++ html/master/setup.php
@@ -7,7 +7,8 @@
   <script src="dist/libs/jquery.js"></script>
   <script src="dist/jstree.min.js"></script>  
   <script src="nodes.js"></script>
-<title>НАСТРОЙКИ УЗЛОВ</title>
+<?php include('../local/local.inc');?>
+<title><?php loc('ms_set_title');?></title>
     
 </head>
 
@@ -20,29 +21,29 @@
 <br>
 <fieldset>
 <div id="fields">
-    <b>Имя узла:</b>&nbsp<input type="text" value="Имя узла" id="name"/>
-    <b>IP адрес [:порт]. По умолчанию:80</b>&nbsp<input type="text" value="IP адрес узла" id="ip"/>
-    <b>Номер АКБ:</b>&nbsp<input type="text" value="Номер АКБ" id="acc_number"/>
-    <b>Подключено:</b>&nbsp<select size=1 id="devices" name="оборудование">
-	<option value="0">МАП</option>
-	<option value="1">MPPT</option>
-	<option value="2">МАП+MPPT</option>
+    <b><?php loc('m_set_name');?>:</b>&nbsp<input type="text" value="" id="name"/>
+    <b>IP&nbsp<?php loc('m_set_address');?></b>&nbsp<input type="text" value="" id="ip"/>
+    <b><?php loc('m_set_acc_no');?>:</b>&nbsp<input type="text" value="" id="acc_number"/>
+    <b><?php loc('m_set_connected');?>:</b>&nbsp<select size=1 id="devices">
+	<option value="0"><?php loc('MAC');?></option>
+	<option value="1"><?php loc('MPPT');?></option>
+	<option value="2"><?php loc('MAC');?>+<?php loc('MPPT');?></option>
     </select> 
 </div>
 
 </fieldset>
 <br>
-<div><input type="button" style="background-color:lightgreen;" value="Создать узел" onclick=create_node() />
+<div><input type="button" style="background-color:lightgreen;" value="<?php loc('m_set_button1');?>" onclick=create_node() />
 
-&nbsp&nbsp<input type="button" style="background-color:orange;" value="Удалить узел" onclick=delete_node() />
+&nbsp&nbsp<input type="button" style="background-color:orange;" value="<?php loc('m_set_button2');?>" onclick=delete_node() />
 
-&nbsp&nbsp<input type="button" style="background-color:lightblue;" value="Сохранить" onclick=save() />
+&nbsp&nbsp<input type="button" style="background-color:lightblue;" value="<?php loc('m_set_button3');?>" onclick=save() />
 
-&nbsp&nbsp<input type="button" style="color:white; background-color:red;" value="Очистить" onclick=erase() /></div>
+&nbsp&nbsp<input type="button" style="color:white; background-color:red;" value="<?php loc('m_set_button4');?>" onclick=erase() /></div>
 <br>
-<div><input type="button" style="background-color:blanchedalmond;" value="Свернуть все" onclick=close_all() />
+<div><input type="button" style="background-color:blanchedalmond;" value="<?php loc('m_set_button5');?>" onclick=close_all() />
 &nbsp&nbsp
-<input type="button" style="background-color:blanchedalmond;" value="Раскрыть все" onclick=open_all() />
+<input type="button" style="background-color:blanchedalmond;" value="<?php loc('m_set_button6');?>" onclick=open_all() />
 
 </div>
 </div>
@@ -50,19 +51,19 @@
 
 <div style="background-color:aquamarine; border: 4px double gray;">
 <fieldset>
-    <b>IP адрес [:порт] узла батарейного монитора:</b>&nbsp<input type="text" value="127.0.0.1" id="batmon_ip"/>
-    <b>Активен:</b>&nbsp<select size=1 id="batmon_active" name="Активен">
-	<option value="0">НЕТ</option>
-	<option value="1">ДА</option>
+    <b>IP <?php loc('m_set_batmon');?>:</b>&nbsp<input type="text" value="127.0.0.1" id="batmon_ip"/>
+    <b><?php loc('m_set_batmon_act');?>:</b>&nbsp<select size=1 id="batmon_active" name="<?php loc('m_set_batmon_act');?>">
+	<option value="0"><?php loc('NO');?></option>
+	<option value="1"><?php loc('YES');?></option>
     </select>
 
 </fieldset>
 <br>
-<input type="button" style="background-color:lightblue;" value="Сохранить" onclick=save_batmon() />
+<input type="button" style="background-color:lightblue;" value="<?php loc('save');?>" onclick=save_batmon() />
 </div>
 <br><br>
 <div>
-<input TYPE="button" style="font-weight:bolder; background-color:darkkhaki;" VALUE=" МЕНЮ " ONCLICK="HomeButton()"> 
+<input TYPE="button" style="font-weight:bolder; background-color:darkkhaki;" VALUE=" <?php loc('MENU');?> " ONCLICK="HomeButton()"> 
 <script>
 function HomeButton()
 {
