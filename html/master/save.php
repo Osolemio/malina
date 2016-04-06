84331a12c6d836786c75e8a13fd7f48ac000a451
Osolemio <osolemio@live.ru>
2016-04-06 14:51:36 +0300

    06042016_14:55 commit

--- html/master/save.php
+++ html/master/save.php
@@ -1,5 +1,6 @@
 <?php
     include("bd.php");
+    include('../local/local.inc');
     
     if ($_POST['truncate']=='true')
 	{
@@ -10,9 +11,9 @@
 
     $query=explode(',',$_POST['node']);
     $dev=0;
-    if ($query[3]=='МАП') $dev=0;
-    if ($query[3]=='MPPT') $dev=1;
-    if ($query[3]=='МАП+MPPT') $dev=2;
+    if ($query[3]==$text['MAC']) $dev=0;
+    if ($query[3]==$text['MPPT']) $dev=1;
+    if ($query[3]==($text['MAC'].'+'.$text['MPPT'])) $dev=2;
     $result=mysql_query("INSERT INTO nodes VALUES (NULL,'".$query[2]."','".$query[0]."','".$dev."','".$query[1]."')",$db) or die(mysql_error());
  mysql_free_result($result);
  mysql_close($db);
